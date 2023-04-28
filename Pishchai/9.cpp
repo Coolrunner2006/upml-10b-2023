2b3524046f7ac45f5c05c201e3b7ddb1ec6e7057
Coolrunner2006 <126869017+Coolrunner2006@users.noreply.github.com>
2023-04-28 10:25:05 +0300

    Create 9.cpp

---
+++ Pishchai/9.cpp
@@ -0,0 +1,29 @@
+include <iostream>
+using namespace std;
+
+int main()
+{
+   
+    int n;
+    cin >> n;
+    
+    
+    int a[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    
+ 
+    int summa = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(a[i] <= 10)
+        {
+            summa += a[i]
+        }
+    }
+    
+   
+    cout << summa << endl;
+}
